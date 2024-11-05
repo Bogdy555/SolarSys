eb4c836a323678726006f8e2abd23d0c37a3a7a3
Bogdy555 <florescubogdan555@gmail.com>
2024-11-05 03:26:40 +0200

    Merged Master into Renderer!

--- SolarFuel/Sources/SolarFuel_Scene.cpp
+++ SolarFuel/Sources/SolarFuel_Scene.cpp
@@ -13,12 +13,12 @@ void SolarFuel::Scene::Entity::DestroyAllChilds()
 	Childs.clear();
 }
 
-glm::mat4 SolarFuel::Scene::Entity::GetLocalMatrix()
+const glm::mat4 SolarFuel::Scene::Entity::GetLocalMatrix() const
 {
 	return glm::translate(glm::mat4(1.0f), glm::vec3(Position.x, Position.y, 0.0f)) * glm::rotate(glm::mat4(1.0f), glm::radians(Angle), glm::vec3(0.0f, 0.0f, 1.0f)) * glm::scale(glm::mat4(1.0f), glm::vec3(Scale.x, Scale.y, 1.0f));
 }
 
-glm::mat4 SolarFuel::Scene::Entity::GetWorldMatrix()
+const glm::mat4 SolarFuel::Scene::Entity::GetWorldMatrix() const
 {
 	if (!Parent)
 	{
